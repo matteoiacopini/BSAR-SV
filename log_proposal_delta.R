f010d9196d72f4a36d08823582bde18e7a8bd778
Matteo Iacopini <matte.iaco@gmail.com>
2025-04-30 12:13:57 +0200

    Delete log_proposal_delta.R

--- log_proposal_delta.R
+++
@@ -1,4 +0,0 @@
-log_proposal_delta <- function(delta, nu){ #not smapling, evaluating
-  log_dens = ddirichlet_R(x = matrix(data = delta, ncol = length(delta)), alpha = nu, log=TRUE)
-  return(log_dens)
-}
